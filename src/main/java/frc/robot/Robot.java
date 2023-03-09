bfd6ae6542fc6839c46b12531367edc186f2eda3
Mickey <mickey_fraser@outlook.com>
2023-03-08 20:56:26 -0600

    Added intake led effect

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -240,14 +240,14 @@ public class Robot extends TimedRobot {
 
   public void intakeLed(double speed, int r, int g, int b){
     for(int i=0; i<13; i++){
-      if(((i+(int)(Timer.getFPGATimestamp()*speed))%6) < 3) {
+      if(Math.abs((i-(int)(Timer.getFPGATimestamp()*speed))%6) < 3) {
         ledsBuffer.setRGB(i, r, g, b);
       } else {
         ledsBuffer.setRGB(i, 0, 0, 0);
       }
     }
     for(int i=13; i<29; i++){
-      if(((i-(int)(Timer.getFPGATimestamp()*speed))%6) < 3) {
+      if(Math.abs((i+(int)(Timer.getFPGATimestamp()*speed))%6) < 3) {
         ledsBuffer.setRGB(i, r, g, b);
       } else {
         ledsBuffer.setRGB(i, 0, 0, 0);
@@ -376,13 +376,13 @@ public class Robot extends TimedRobot {
       intakePower = INTAKE_OUTPUT_POWER;
       intakeAmps = INTAKE_CURRENT_LIMIT_A;
       lastGamePiece = CUBE;
-      intakeLed(3.0, 153, 0, 255);
+      intakeLed(20.0, 153, 0, 255);
     } else if (j2.getRawButton(3)) {
       // cone in or cube out
       intakePower = -INTAKE_OUTPUT_POWER;
       intakeAmps = INTAKE_CURRENT_LIMIT_A;
       lastGamePiece = CONE;
-      intakeLed(3.0, 255, 204, 0);
+      intakeLed(20.0, 255, 204, 0);
     } else if (lastGamePiece == CUBE) {
       intakePower = INTAKE_HOLD_POWER;
       intakeAmps = INTAKE_HOLD_CURRENT_LIMIT_A;
@@ -409,6 +409,10 @@ public class Robot extends TimedRobot {
       setDriveMotors(j.getRawAxis(0)/3.5, j.getRawAxis(1)/3.0);
     } else {
       setDriveMotors(j.getRawAxis(0)/2.2, j.getRawAxis(1)/1.5);
+    }
+
+    if (j.getRawButton(3)){
+      setDriveMotors(0.0, 0.2);
     }
   }
 }
