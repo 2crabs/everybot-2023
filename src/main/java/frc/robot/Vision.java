3b5c837b53e456895fb762717592126d5be4e981
Mickey <mickey_fraser@outlook.com>
2023-03-22 19:28:50 -0500

    Added i2c communication for vision

---
+++ src/main/java/frc/robot/Vision.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.I2C;
+
+import java.nio.ByteBuffer;
+
+public class Vision {
+    private I2C Wire;
+    public int val;
+
+    public Vision() {
+        //Address of controller is 0
+        Wire = new I2C(I2C.Port.kMXP, 0);
+    }
+
+    public void update(){
+        int numBytes = 1;
+        byte[] data = new byte[numBytes];
+        //Address of peripheral
+        Wire.read(1, numBytes, data);
+        val = data[0];
+    }
+}
